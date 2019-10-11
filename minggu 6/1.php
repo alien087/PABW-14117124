fb1bae201be666d1493b89ee4539f6a781f3a7af
alien087 <44940977+alien087@users.noreply.github.com>
2019-10-11 10:39:10 +0700

    Percobaan 1,2 dan Latihan 1

---
+++ minggu 6/1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <title>Contoh Skrip PHP</title>
+
+<body>
+
+
+
+<?php
+  $a = 2;
+  $hasil = ($a<4) ? ($a*2) : ($a*3);
+  echo "Hasilnya adalah $hasil";
+?>
+
+</body>
+</html>
